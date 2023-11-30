fe4ba6fd325fe61dbf0462d46ad411b26b8090b9
MhdRizky7 <mhdrizky553@gmail.com>
2023-11-30 09:24:58 +0700

    first commit

--- app/models/Pelanggan.php
+++ app/models/Pelanggan.php
@@ -61,37 +61,27 @@ public function edit($id)
 
 public function update()
 {
-    $pel_id_gol = $_POST['pel_id_gol'];
-    $pel_no = $_POST['pel_no'];
-    $pel_nama = $_POST['pel_nama'];
-    $pel_alamat = $_POST['pel_alamat'];
-    $pel_hp = $_POST['pel_hp'];
-    $pel_ktp = $_POST['pel_ktp'];
-    $pel_seri = $_POST['pel_seri'];
-    $pel_meteran = $_POST['pel_meteran'];
-    $pel_aktif = $_POST['pel_aktif'];
-    $pel_id_user = $_POST['pel_id_user'];
-    $id = $_POST['id'];
+               $pel_id_gol = $_POST['gol_id'];
+          $pel_nama = $_POST['pel_nama'];
+          $pel_alamat = $_POST['pel_alamat'];
+          $pel_hp = $_POST['pel_hp'];
+          $pel_aktif = $_POST['pel_aktif'];
+          $pel_id = $_POST['id'];
 
-    $sql = "UPDATE tb_pelanggan
-            SET pel_id_gol=:pel_id_gol, pel_no=:pel_no, pel_nama=:pel_nama, pel_alamat=:pel_alamat, pel_hp=:pel_hp, pel_ktp=:pel_ktp, pel_seri=:pel_seri, pel_meteran=:pel_meteran, pel_aktif=:pel_aktif, pel_id_user=:pel_id_user
-            WHERE pel_id=:pel_id";
 
-    $stmt = $this->db->prepare($sql);
+          $sql = "UPDATE tb_pelanggan
+          SET pel_id_gol=:pel_id_gol, pel_nama=:pel_nama, pel_alamat=:pel_alamat, pel_hp=:pel_hp,  pel_aktif=:pel_aktif WHERE pel_id=:pel_id";
 
-    $stmt->bindParam(":pel_id_gol", $pel_id_gol);
-    $stmt->bindParam(":pel_no", $pel_no);
-    $stmt->bindParam(":pel_nama", $pel_nama);
-    $stmt->bindParam(":pel_alamat", $pel_alamat);
-    $stmt->bindParam(":pel_hp", $pel_hp);
-    $stmt->bindParam(":pel_ktp", $pel_ktp);
-    $stmt->bindParam(":pel_seri", $pel_seri);
-    $stmt->bindParam(":pel_meteran", $pel_meteran);
-    $stmt->bindParam(":pel_aktif", $pel_aktif);
-    $stmt->bindParam(":pel_id_user", $pel_id_user);
-    $stmt->bindParam(":pel_id", $id);
+          $stmt = $this->db->prepare($sql);
 
-    $stmt->execute();
+          $stmt->bindParam(":pel_id_gol", $pel_id_gol);
+          $stmt->bindParam(":pel_nama", $pel_nama);
+          $stmt->bindParam(":pel_alamat", $pel_alamat);
+          $stmt->bindParam(":pel_hp", $pel_hp);
+          $stmt->bindParam(":pel_aktif", $pel_aktif);
+          $stmt->bindParam(":pel_id", $pel_id);
+
+          $stmt->execute();
 }
 
 public function delete($id)
